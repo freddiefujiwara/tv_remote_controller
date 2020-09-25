84f4bc46ea3836deba7bc3777d588a4cefa1e84e
Fumikazu Fujiwara <github@ze.gs>
2020-09-26 06:20:27 +0900

    add tv icon

--- lib/main.dart
+++ lib/main.dart
@@ -23,30 +23,249 @@ class TVRemoteController extends StatelessWidget {
                 title: Text('TV Remote Controller'),
               ),
               body: Consumer<TVModel>(builder: (context, model, child) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'You have pushed the button this many times:',
-                      ),
-                      Text(
-                        '${model.counter}',
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {
-                          model.increment();
-                        },
-                        tooltip: 'Increment',
-                        child: Icon(Icons.add),
-                      ),
-                      // This trailing comma makes auto-formatting nicer for build methods.
-                    ],
+                return SafeArea(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.power_settings_new,
+                                  color: Colors.red, size: 30),
+                              onPressed: () async {
+                                await model.power();
+                              },
+                            ),
+                          ],
+                        ),
+                        Visibility(
+                          visible: true,
+                          child: Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    FloatingActionButton(
+                                        child: Text(
+                                          "1",
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white),
+                                        ),
+                                        onPressed: () async {
+                                          await model.channel(1);
+                                        }),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "2",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(2);
+                                      },
+                                    ),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "3",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(3);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "4",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(4);
+                                      },
+                                    ),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "5",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(5);
+                                      },
+                                    ),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "6",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(6);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "7",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(7);
+                                      },
+                                    ),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "8",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.channel(8);
+                                      },
+                                    ),
+                                    FloatingActionButton(
+                                      child: Text(
+                                        "M",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () async {
+                                        await model.menu();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Visibility(
+                          visible: true,
+                          child: Expanded(
+                            child: Stack(
+                              children: [
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_circle_up,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.volumeUp();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_circle_down,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.volumeDown();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: FloatingActionButton(
+                                    child: Text(
+                                      "OK",
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    onPressed: () async {
+                                      await model.ok();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment(0, -0.6),
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_drop_up,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.up();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment(0, 0.6),
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_drop_down,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.down();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment(0.6, 0),
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_right,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.right();
+                                    },
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment(-0.7, 0),
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.arrow_left,
+                                        size: 30, color: Colors.white),
+                                    onPressed: () async {
+                                      await model.left();
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               })),
-        )
-    );
+        ));
   }
 }
--- lib/tv_model.dart
+++ lib/tv_model.dart
@@ -1,12 +1,67 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TVModel extends ChangeNotifier {
+  final baseUrl = "http://a.ze.gs/broadlinkjs-rm-cli/34:ea:34:42:a9:66";
   int counter = 0;
-  void increment(){
-    this.counter++;
-    print('${this.counter}');
+  void power() async {
+    await http.get("${this.baseUrl}/tv");
+    notifyListeners();
+  }
+
+  void channel(key) async {
+    await http.get("${this.baseUrl}/tv_${key}");
+    print('CHANNEL: ${key}');
+    notifyListeners();
+  }
+
+  void menu() async {
+    await http.get("${this.baseUrl}/tv_menu");
+    print('MENU');
+    notifyListeners();
+  }
+
+  void volumeUp() async {
+    await http.get("${this.baseUrl}/tv_vol_up");
+    print('VOLUME UP');
+    notifyListeners();
+  }
+
+  void volumeDown() async {
+    await http.get("${this.baseUrl}/tv_vol_down");
+    print('VOLUME DOWN');
+    notifyListeners();
+  }
+
+  void ok() async {
+    await http.get("${this.baseUrl}/tv_submit");
+    print('OK');
+    notifyListeners();
+  }
+
+  void up() async {
+    await http.get("${this.baseUrl}/tv_up");
+    print('UP');
+    notifyListeners();
+  }
+
+  void down() async {
+    await http.get("${this.baseUrl}/tv_down");
+    print('DOWN');
+    notifyListeners();
+  }
+
+  void left() async {
+    await http.get("${this.baseUrl}/tv_left");
+    print('LEFT');
+    notifyListeners();
+  }
+
+  void right() async {
+    await http.get("${this.baseUrl}/tv_right");
+    print('RIGHT');
     notifyListeners();
   }
 }
