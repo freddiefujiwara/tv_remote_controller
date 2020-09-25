c5f923851922bb49dce3ba2453740732c53af522
Fumikazu Fujiwara <github@ze.gs>
2020-09-26 06:39:33 +0900

    add base_url

--- lib/tv_model.dart
+++ lib/tv_model.dart
@@ -1,67 +1,76 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class TVModel extends ChangeNotifier {
-  final baseUrl = "http://a.ze.gs/broadlinkjs-rm-cli/34:ea:34:42:a9:66";
   int counter = 0;
   void power() async {
-    await http.get("${this.baseUrl}/tv");
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv");
+    print('POWER: ${baseUrl.trim()}/tv');
     notifyListeners();
   }
 
   void channel(key) async {
-    await http.get("${this.baseUrl}/tv_${key}");
-    print('CHANNEL: ${key}');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_${key}");
+    print('CHANNEL: ${key} ${baseUrl.trim()}/tv_${key}');
     notifyListeners();
   }
 
   void menu() async {
-    await http.get("${this.baseUrl}/tv_menu");
-    print('MENU');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_menu");
+    print('MENU: ${baseUrl.trim()}/tv_menu');
     notifyListeners();
   }
 
   void volumeUp() async {
-    await http.get("${this.baseUrl}/tv_vol_up");
-    print('VOLUME UP');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_vol_up");
+    print('VOLUME UP: ${baseUrl.trim()}/tv_vol_up');
     notifyListeners();
   }
 
   void volumeDown() async {
-    await http.get("${this.baseUrl}/tv_vol_down");
-    print('VOLUME DOWN');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_vol_down");
+    print('VOLUME DOWN: ${baseUrl.trim()}/tv_vol_down');
     notifyListeners();
   }
 
   void ok() async {
-    await http.get("${this.baseUrl}/tv_submit");
-    print('OK');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_submit");
+    print('OK: ${baseUrl.trim()}/tv_submit');
     notifyListeners();
   }
 
   void up() async {
-    await http.get("${this.baseUrl}/tv_up");
-    print('UP');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_up");
+    print('UP: ${baseUrl.trim()}/tv_up');
     notifyListeners();
   }
 
   void down() async {
-    await http.get("${this.baseUrl}/tv_down");
-    print('DOWN');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_down");
+    print('DOWN: ${baseUrl.trim()}/tv_down');
     notifyListeners();
   }
 
   void left() async {
-    await http.get("${this.baseUrl}/tv_left");
-    print('LEFT');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_left");
+    print('LEFT: ${baseUrl.trim()}/tv_left');
     notifyListeners();
   }
 
   void right() async {
-    await http.get("${this.baseUrl}/tv_right");
-    print('RIGHT');
+    String baseUrl = await rootBundle.loadString('./base_url.txt');
+    await http.get("${baseUrl.trim()}/tv_right");
+    print('RIGHT: ${baseUrl.trim()}/tv_right');
     notifyListeners();
   }
 }
